6bba4c57044a94449a0c87df8ebea484bd197e71
AflDavid <93366100+AflDavid@users.noreply.github.com>
2022-02-07 10:43:27 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿
+using System;
+
+class Test
+{
+
+	static int[] arr = { 1, 2, 3, 4, 5 };
+
+
+	static int findSum(int[] A, int N)
+	{
+		if (N <= 0)
+			return 0;
+		return (findSum(A, N - 1) + A[N - 1]);
+	}
+
+	public static void Main()
+	{
+		Console.Write(findSum(arr, arr.Length));
+	}
+}
